cf6e486ddfc00bdf7b5df7ce7848766aba5d8cc5
Tyler Smith <tylersmith1357@icloud.com>
2021-11-04 15:02:00 -0400

    Implemented CONNECT command

--- client.py
+++ client.py
@@ -13,26 +13,57 @@ def listening_fn(conn: socket) -> None:
 def talking_fn(conn: socket) -> None:
     while True:
         message = input("Enter Message: ")
-        conn.send(message.encode("utf-8"))
-        if message == "exit":
+        word_list = message.split()
+        if not word_list:
+            messageError()
+        elif word_list[0] == "EXIT":
             break
+        elif word_list[0] == "UPLOAD":
+            conn.send(word_list[1].encode("utf-8"))
+        else:
+            messageError()
 
+# Error function for communicating with server
+def messageError() -> None:
+    print("Please enter a valid command: UPLOAD filename or DOWNLOAD filename")
+    print("Or the command: EXIT to exit the client")
 
-def main(hostname: str, portno: int) -> None:
-    conn = socket(AF_INET, SOCK_STREAM)
-    conn.connect((hostname, portno))
+# Error function for command
+def commandError() -> None:
+    print("Please enter the command: CONNECT server_IP_address server_port to connect to server")
+    print("Or the command: EXIT to exit the client")
 
-    listening_thread = Thread(target=listening_fn, args=(conn,))
-    talking_thread = Thread(target=talking_fn, args=(conn,))
+def main() -> None:
+    while True:
+        # Get command from user
+        command = input("Enter Command: ")
+        word_list = command.split()
 
-    listening_thread.start()
-    talking_thread.start()
+        # Determine if the command is acceptable
+        if not word_list:
+            commandError()
+        elif word_list[0] == "EXIT":
+            exit()
+        elif len(word_list) < 3:
+            commandError()
+        elif word_list[0] == "CONNECT":
+            server_IP = word_list[1]
+            server_port = int(word_list[2])
+            conn = socket(AF_INET, SOCK_STREAM)
+            conn.connect((server_IP, server_port))
 
-    listening_thread.join()
-    talking_thread.join()
+            listening_thread = Thread(target=listening_fn, args=(conn,))
+            talking_thread = Thread(target=talking_fn, args=(conn,))
 
-    conn.close()
+            listening_thread.start()
+            talking_thread.start()
 
+            listening_thread.join()
+            talking_thread.join()
+
+            conn.close()
+        else:
+            commandError()
 
 if __name__ == "__main__":
-    main("localhost", 8080)
+    main()
